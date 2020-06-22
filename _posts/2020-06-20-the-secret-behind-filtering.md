60d2b4666892268d4cd6d41bd982111678fc9385
Jan Wilczek <janekw@vp.pl>
2020-06-22 20:09:41 +0200

    WIP: 3D plot of filter's outputs.

--- _posts/2020-06-20-the-secret-behind-filtering.md
+++ _posts/2020-06-20-the-secret-behind-filtering.md
@@ -3,7 +3,8 @@ title: "Convolution: The secret behind filtering"
 date: 2020-06-20
 author: Jan Wilczek
 layout: post
-permalink: /convolution-secret-behind-filtering/
+permalink: /convolution-the-secret-behind-filtering/
+images: assets/img/posts/2020-06-20-the-secret-behind-filtering
 background:
 categories:
  - DSP
@@ -31,8 +32,71 @@ Are You ready?
 
 In its simplest form the convolution between two discrete signals $x[n]$ and $h[n]$ can be expressed as an **infinite sum**:
 
-$$ x[n] \ast h[n] = \sum_{k=-\infty}^{\infty} x[k] h[n - k] = y[n] $$.
+$$ x[n] \ast h[n] = \sum_{k=-\infty}^{\infty} x[k] h[n - k] = y[n]. $$
 
 Whoa, what's happened here? Under the sum we have the two signals, but the second one is not only **shifted in time by $n$**, but also **time-reversed**!
+
+# Intuition
+
+## Filtering
+
+Let's consider the above equation with $h[n]$ denoting filter's (or any linear time-invariant (LTI) system) impulse response and $x[n]$ as this filter's input signal. From signal processing we know, that any LTI system is completely specified by its impulse response.
+
+If we denote the output of the filter by $y[n]$ we may look at the output as a weighted sum of filter's impulse responses. How?
+
+Consider $n=0$. At the output we get
+
+$$y[0] = \sum_{k=-\infty}^{\infty} x[k] h[0 - k] = \sum_{k=-\infty}^{\infty} x[k] h[- k] = x[0] h[0],$$ because $h[n] = 0$ and $x[n]=0$ for all $n < 0$. What do we get for $n=1$?
+
+$$y[1] = \sum_{k=-\infty}^{\infty} x[k] h[1 - k] = x[0]h[1] + x[1]h[0].$$ As you can see, x[0] has moved "further down the road" (further into the filter's buffer) and now constitutes the weight for $h[1]$ from filter's impulse response. At the same time $x[1]$ enters the buffer and (as $x[0]$ previously) weights the $h[0]$. The operation repeats for every following input sample. $x[0]$ stops weighting filter's impulse response when it has weighted the last one of them (unless it is an IIR filter; it then weights the filter's impulse response infinitely).
+
+## Another view
+
+We can also look at that operation from a different perspective. What if we fix $k$? In this case it describes the behaviour of the system if only input sample $x[k]$ was given:
+
+$$ y_k[n] = x[k]h[n-k].$$
+
+The above equation basically says, that once $x[k]$ enters the filter, it will weigh its entire impulse response delayed by $k$ samples w.r.t $n$. We then just have to sum up over all possible $k$ to conclude, that $y[n]$ is just filter's impulse response, delayed and weighted by $x[n]$.
+
+This may all get a little bit confusing at this moment, so let's look at an example, shall we?
+
+# Example
+
+Let's consider the following signal $x[n]$:
+
+![]({{ page.images | absolute_url | append: "/x.png"}})
+
+and filter's impulse response $h[n]$:
+
+![]({{ page.images | absolute_url | append: "/h.png"}})
+
+The result of their convolution is the following signal $y[n]$ (filter's output):
+
+![]({{ page.images | absolute_url | append: "/y.png"}})
+
+Not very meaningful, is it? The only thing that we can observe is that output's length is the sum of input's and filter's lengths minus one.
+
+Let's try some color coding. We can depict each of $x[n]$'s samples in a different color:
+
+![]({{ page.images | absolute_url | append: "/x_single.png"}})
+
+We can now examine the impact of particular samples on the filter's output. What would happen if only  blue $x[0]$ entered the filter?
+
+![]({{ page.images | absolute_url | append: "/h_single_0.png"}})
+
+We can see, that the entire impulse response of the filter got scaled by $x[0]$ which in this case is equal to $0.1$.
+
+Now, let's imagine, that only second sample, namely orange $x[1]$, entered the filter. What could we observe at the output?
+
+![]({{ page.images | absolute_url | append: "/h_single_1.png"}})
+
+Notice that at $n=0$ the filter's output is $0$, because at that time $x[1]$ has not yet entered the filter. But from $n=1$ onwards we get again filter's impulse response scaled by the newly entering sample.
+
+The same thing happens for green $x[2]$ and red $x[3]$:
+![]({{ page.images | absolute_url | append: "/h_single_2.png"}})
+![]({{ page.images | absolute_url | append: "/h_single_3.png"}})
+
+Summing all these "partial" responses up we obtain:
+
 
 {% endkatexmm %}
--- _py/posts/2020-06-20-the-secret-behind-filtering/figures.py
+++ _py/posts/2020-06-20-the-secret-behind-filtering/figures.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 import numpy as np
+import numpy.matlib as mtlb
 import matplotlib.pyplot as plt
 from pathlib import Path
+import mpl_toolkits.mplot3d.art3d as art3d
 
 
-IMG_DIR = '../..'
+COLORS = ['C0','C1','C2','C3']
 
-x = np.array([0.1, 0.6, 0.4, 0.2])
-h = np.array([0.7, 0.5, 0.3])
 
 def get_assets_path():
     path = Path.cwd()
-    while path.name != 'assets':
+    while path.name != '_py':
         path = path.parent
-    return path
+    return path.parent
 
-def plot_signal(signal, filepath):
+def plot_signal(signal, filepath, color='k'):
     xlim = [-1, 7]
 
     plt.figure()
-    plt.stem(x, use_line_collection=True, basefmt=' ', markerfmt='ok', linefmt='k-')
+    plt.stem(signal, use_line_collection=True, basefmt=' ', markerfmt=f'{color}o', linefmt=f'{color}-')
     plt.xlim(xlim)
     plt.ylim([-1.1, 1.1])
     plt.yticks(np.arange(-1,1.1,0.25), labels=['-1','','','','0','','','','1'])
@@ -36,17 +36,102 @@ def plot_signal(signal, filepath):
     # Show ticks in the left and lower axes only
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    # plt.savefig(filepath.absolute(), bbox_inches='tight', dpi=300)
-    plt.show()
+    plt.savefig(filepath.absolute(), bbox_inches='tight', dpi=300)
+    # plt.show()
+
+def plot_signals(signals, filepath):
+    xlim = [-1, 7]
+
+
+    plt.figure()
+    for i in range(signals.shape[0]):
+        plt.stem([i], [signals[i,i]], use_line_collection=True, basefmt=' ', markerfmt=f'{COLORS[i]}o', linefmt=f'{COLORS[i]}-')
+    plt.xlim(xlim)
+    plt.ylim([-1.1, 1.1])
+    plt.yticks(np.arange(-1,1.1,0.25), labels=['-1','','','','0','','','','1'])
+    # plt.grid(b=True)
+    
+    ax = plt.gca()
+
+    # Move left y-axis and bottim x-axis to centre, passing through (0,0)
+    ax.spines['left'].set_position(('data', 0.0))
+    ax.spines['bottom'].set_position(('data', 0.0))
+
+    # Eliminate upper and right axes
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    # Show ticks in the left and lower axes only
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    plt.savefig(filepath.absolute(), bbox_inches='tight', dpi=300)
+    # plt.show()
+
+def plot_signals_3d(signals, filepath):
+    xlim = [-1, 7]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    for i in range(signals.shape[0]):
+        depth = 1 - signals.shape[0] + i
+        for j in range(signals.shape[1]):
+            line = art3d.Line3D(*zip((j, depth, 0), (j, depth, signals[i,j])), marker='o', markevery=(1, 1))
+            ax.add_line(line)
+    plt.xlim(xlim)
+    plt.ylim([-1.1, 1.1])
+    plt.yticks(np.arange(-1,1.1,0.25), labels=['-1','','','','0','','','','1'])
+    # plt.grid(b=True)
+    
+    ax = plt.gca()
+
+    # Move left y-axis and bottim x-axis to centre, passing through (0,0)
+    # ax.spines['left'].set_position(('data', 0.0))
+    # ax.spines['bottom'].set_position(('data', 0.0))
+
+    # Eliminate upper and right axes
+    # ax.spines['right'].set_color('none')
+    # ax.spines['top'].set_color('none')
+
+    # Show ticks in the left and lower axes only
+    # ax.xaxis.set_ticks_position('bottom')
+    # ax.yaxis.set_ticks_position('left')
+    plt.savefig(filepath.absolute(), bbox_inches='tight', dpi=300)
+    # plt.show()
 
 def main():
     assets_path = get_assets_path()
-    post_images_path = assets_path.joinpath('/img/posts/2020-06-20-the-secret-behind-filtering')
+    post_images_path = assets_path.joinpath('assets/img/posts/2020-06-20-the-secret-behind-filtering')
     filepath = post_images_path.joinpath('x.png')
+
+    x = np.array([0.1, 0.6, 0.4, 0.2])
+    h = np.array([0.7, 0.3, 0.5])
 
     y = np.convolve(x, h)
 
     plot_signal(x, filepath)
+    plot_signal(h, post_images_path.joinpath('h.png'))
+    plot_signal(y, post_images_path.joinpath('y.png'))
+
+    x_single = np.zeros((len(x),len(x)))
+    for i in range(len(x)):
+        x_single[i,i] = x[i]
+
+    plot_signals(x_single, post_images_path.joinpath('x_single.png'))
+
+    h_single = np.zeros((len(x),len(y)))
+    # h_domain_visualization = mtlb.repmat(np.arange(0.0,len(y),1.0), h_single.shape[0], 1)
+    for i in range(len(x)):
+        h_single[i,:] = np.convolve(x_single[i,:],h)
+        plot_signal(h_single[i,:], post_images_path.joinpath(f'h_single_{i}.png'), color=COLORS[i])
+
+        # SHIFT_FACTOR = 0.1
+        # h_domain_visualization[i,:] =  np.add(h_domain_visualization[i,:], SHIFT_FACTOR * (1 - len(x) + i))
+    
+    # print(h_domain_visualization)
+    plot_signals_3d(h_single, post_images_path.joinpath('h_superposed.png'))
+    
+    
+    
 
     
 
