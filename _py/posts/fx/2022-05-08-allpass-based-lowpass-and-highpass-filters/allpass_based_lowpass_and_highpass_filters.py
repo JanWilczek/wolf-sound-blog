01d4a4d3fea65b9dfd5d4e5730eb4067d1af84f2
Jan Wilczek <janekw@vp.pl>
2022-05-08 13:52:27 +0200

    Posts: Allpass-based LP and HP.

--- _py/posts/fx/2022-05-08-allpass-based-lowpass-and-highpass-filters/allpass_based_lowpass_and_highpass_filters.py
+++ _py/posts/fx/2022-05-08-allpass-based-lowpass-and-highpass-filters/allpass_based_lowpass_and_highpass_filters.py
@@ -8,31 +8,22 @@ def generate_white_noise(duration_in_seconds, sampling_rate):
     duration_in_samples = int(duration_in_seconds * sampling_rate)
     return np.random.default_rng().uniform(-1, 1, duration_in_samples)
 
-def main():
-    sampling_rate = 44100
-    duration_in_seconds = 5
-    white_noise = generate_white_noise(duration_in_seconds, sampling_rate)
-    highpass = False
-    amplitude = 0.1
+def a1_coefficient(break_frequency, sampling_rate):
+    tan = np.tan(np.pi * break_frequency / sampling_rate)
+    return (tan - 1) / (tan + 1)
 
-    input_signal = white_noise
-
-    cutoff_frequency = np.geomspace(20000, 20, input_signal.shape[0])
-
-    # b = [a1, 1]
-    # a = [1, a1]
-
-    # allpass_output = signal.lfilter(b, a, input_signal)
+def allpass_filter(input_signal, break_frequency, sampling_rate):
     allpass_output = np.zeros_like(input_signal)
     dn_1 = 0
     for n in range(input_signal.shape[0]):
-        break_frequency = cutoff_frequency[n]
-        
-        tan = np.tan(np.pi * break_frequency / sampling_rate)
-        a1 = (tan - 1) / (tan + 1)
+        a1 = a1_coefficient(break_frequency[n], sampling_rate)
 
         allpass_output[n] = a1 * input_signal[n] + dn_1
         dn_1 = input_signal[n] - a1 * allpass_output[n]
+    return allpass_output
+
+def allpass_based_filter(input_signal, cutoff_frequency, sampling_rate, highpass=False, amplitude=1.0):
+    allpass_output = allpass_filter(input_signal, cutoff_frequency, sampling_rate)
 
     if highpass:
         allpass_output *= -1
@@ -40,6 +31,18 @@ def main():
     filter_output = input_signal + allpass_output
     filter_output *= 0.5
     filter_output *= amplitude
+
+    return filter_output
+
+def main():
+    sampling_rate = 44100
+    duration_in_seconds = 5
+    white_noise = generate_white_noise(duration_in_seconds, sampling_rate)
+    input_signal = white_noise
+
+    cutoff_frequency = np.geomspace(20000, 20, input_signal.shape[0])
+
+    filter_output = allpass_based_filter(input_signal, cutoff_frequency, sampling_rate, highpass=False, amplitude=0.1)
 
     filename = 'filtered_white_noise.flac'
 
