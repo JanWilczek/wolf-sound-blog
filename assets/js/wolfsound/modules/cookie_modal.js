e80dc2fe35108b4d3ea118fb2fb83d649309fb3e
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-07-22 09:37:15 +0200

    Fixed cookie popup display

--- assets/js/wolfsound/modules/cookie_modal.js
+++ assets/js/wolfsound/modules/cookie_modal.js
@@ -1,15 +1,13 @@
 import Cookify from '/assets/js/cookify/index.js'
     function handle_cookies(){
-        var cookieModal = document.getElementById("cookieModal")
+        var cookieModal = document.getElementById("cookieBanner")
         var cookify = new Cookify('cookie_consent', function () {
-            // document.getElementById('manage').classList.add('d-none')
             cookieModal.style.display = "none";
         }, function (data) {
             console.log(data)
         }, false, false, 'necessary')
 
         if (!cookify.getDataState(cookify.viewedName)) {
-            // document.getElementById('manage').classList.remove('d-none')
             cookieModal.style.display = "block"
         }  
     }
