09ee45dc411dfe859c008ad4a243dc8a42a1b46c
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-07-22 09:37:54 +0200

    Reformatting

--- assets/js/wolfsound/modules/cookie_modal.js
+++ assets/js/wolfsound/modules/cookie_modal.js
@@ -1,15 +1,22 @@
-import Cookify from '/assets/js/cookify/index.js'
-    function handle_cookies(){
-        var cookieModal = document.getElementById("cookieBanner")
-        var cookify = new Cookify('cookie_consent', function () {
-            cookieModal.style.display = "none";
-        }, function (data) {
-            console.log(data)
-        }, false, false, 'necessary')
+import Cookify from "/assets/js/cookify/index.js";
 
-        if (!cookify.getDataState(cookify.viewedName)) {
-            cookieModal.style.display = "block"
-        }  
-    }
-    window.addEventListener('load', handle_cookies)
-    
+function handle_cookies() {
+  var cookieModal = document.getElementById("cookieBanner");
+  var cookify = new Cookify(
+    "cookie_consent",
+    function () {
+      cookieModal.style.display = "none";
+    },
+    function (data) {
+      console.log(data);
+    },
+    false,
+    false,
+    "necessary"
+  );
+
+  if (!cookify.getDataState(cookify.viewedName)) {
+    cookieModal.style.display = "block";
+  }
+}
+window.addEventListener("load", handle_cookies);
