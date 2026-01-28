b367809a4579f3afc99b9f3473a53d109575a1ef
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-01-28 08:51:55 +0100

    Removed smart navbar on mobile

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,13 +12,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
     window.addEventListener('scroll', function() {
+        if (window.innerWidth < 1200) {
+            return;
+        }
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
             } else {
-                console.log(123);
                 mainNav.classList.remove('is-visible', 'is-fixed');
             }
         } else {
